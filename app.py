9c2b3b8c97fa95ba67744dd0e1aae60505b81f1e
swaroopedupulapati <swaroopedupulapati1@gmail.com>
2024-11-09 12:14:44 +0530

    qis day1

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask,render_template,request
+
+app = Flask(__name__)
+@app.route('/', methods=['GET'])
+def home():
+    return render_template("index.html")
+
+
+
+app.run(debug=True)
