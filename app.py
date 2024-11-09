333f90f873ed8d6eeaa7e1864b6372dfa77e9fe8
swaroopedupulapati <swaroopedupulapati1@gmail.com>
2024-11-09 19:30:07 +0530

    qis day1

--- app.py
+++ app.py
@@ -6,5 +6,16 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/callme",methods=['GET',"POST"])
+def callme_details():
+    name=request.form("name")
+    email=request.form("email")
+    ph_no=request.form("phone_number")
+    course=request.form("course")
+    return f""" mr {name} your details was updated check once again
+            name = {name}
+            email = {email}
+            phone.no ={ph_no}
+            course = {course} """
 
 app.run(debug=True)
